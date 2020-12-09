96fa70408d61265f9da7035f681b9d7a679d88c2
Alexsander <alexsander.rodrigues@gmail.com>
2020-12-09 19:27:11 -0300

    Fixed shellcheck issue

--- k8s_profile.sh
+++ k8s_profile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 k8sConfigFolder=~/.kube
-k8sConfig=1
-select k8sFile in $(ls ${k8sConfigFolder}|grep yml ) ; do
-	export KUBECONFIG=${k8sFile}
+select k8sFile in $(ls ${k8sConfigFolder}/*.yml | xargs -n 1 basename ) ; do
+	export KUBECONFIG=${k8sConfigFolder}/${k8sFile}
 	echo -e "\e[33mKUBECONFIG:\e[37m ${KUBECONFIG} \U0002714"
 	return
 done

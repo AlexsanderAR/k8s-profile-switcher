519f6b5a298721fed0bb965545ff99706c42f840
Alexsander <alexsander.rodrigues@gmail.com>
2020-12-09 19:32:29 -0300

    replace ls by a find

--- k8s_profile.sh
+++ k8s_profile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 k8sConfigFolder=~/.kube
-select k8sFile in $(ls ${k8sConfigFolder}/*.yml | xargs -n 1 basename ) ; do
+#select k8sFile in $(ls ${k8sConfigFolder}/*.yml | xargs -n 1 basename ) ; do
+select k8sFile in $(find ~/.kube/ -iname "*.yml" -exec basename {} \;) ; do
 	export KUBECONFIG=${k8sConfigFolder}/${k8sFile}
 	echo -e "\e[33mKUBECONFIG:\e[37m ${KUBECONFIG} \U0002714"
 	return

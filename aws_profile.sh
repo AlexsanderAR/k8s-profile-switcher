98f4158583c83b303d3bb42b9ba64d0308c3e494
Alexsander <alexsander.rodrigues@gmail.com>
2021-01-14 16:03:35 -0300

    Fixed grep run

--- aws_profile.sh
+++ aws_profile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-select awsProfile in $(fgrep \[ ~/.aws/config | sed 's/^.* //g;s/.$//g') ; do
+select awsProfile in $(grep -F \[ ~/.aws/config | sed 's/^.* //g;s/.$//g') ; do
 	export AWS_PROFILE=${awsProfile}
 	echo -e "\e[33mAWS_PROFILE:\e[37m ${awsProfile} \U0002714"
 	return

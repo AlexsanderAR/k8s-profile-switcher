addf9a0dfe9c7bd9e1bd969008ca8d31ae064f28
Alexsander <alexsander.rodrigues@bairesdev.com>
2022-01-12 13:01:16 -0300

    Fixed quote issue

--- aws_profile.sh
+++ aws_profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #select awsProfile in $(grep -F \[ ~/.aws/config | sed 's/^.* //g;s/.$//g') ; do
 select awsProfile in $(grep -F \[ ~/.aws/credentials | sed 's/\[//g;s/\]//g') ; do
-	saml2aws login -a ${awsProfile}
+	saml2aws login -a "${awsProfile}"
 	export AWS_PROFILE=${awsProfile}
 	echo -e "\e[33mAWS_PROFILE:\e[37m ${awsProfile} \U0002714"
 	return

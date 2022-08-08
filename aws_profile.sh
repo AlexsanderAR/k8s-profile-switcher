619dd6362271463aa752dfa662e47506b90b985e
Alexsander Rodrigues <alexsander.rodrigues@gmail.com>
2022-08-08 09:41:43 -0300

    update

--- aws_profile.sh
+++ aws_profile.sh
@@ -2,6 +2,9 @@
 #select awsProfile in $(grep -F \[ ~/.aws/config | sed 's/^.* //g;s/.$//g') ; do
 select awsProfile in $(grep -F \[ ~/.aws/credentials | sed 's/\[//g;s/\]//g') ; do
 	saml2aws login -a "${awsProfile}"
+        if [ ${awsProfile} == 'consensus' ] ; then
+	   awsProfile='consensus-dev'
+        fi
 	export AWS_PROFILE=${awsProfile}
 	echo -e "\e[33mAWS_PROFILE:\e[37m ${awsProfile} \U0002714"
 	return
--- git_profile.sh
+++ git_profile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-keyFolder=~/.ssh
-select gitKey in $(find ~/.ssh/ -iname "id_rsa-*" -exec basename {} \;) ; do
-        export GIT_SSH_COMMAND="ssh -i ${keyFolder}/${gitKey}"
-	echo -e "\e[33mGIT-KEY:\e[37m ${gitKey} \U0002714"
-	return
+select userAccount in personal procore consensus ; do
+  [ ${userAccount} == "personal" ]  && userEmail=alexsander.rodrigues@gmail.com
+  [ ${userAccount} == "procore" ]   && userEmail=alexsander.rodrigues-contractor@procore.com
+  [ ${userAccount} == "consensus" ] && userEmail=alexsander.rodrigues@consensus.com
+  git config --global user.email "${userEmail}"
+  git config --global user.name "Alexsander Rodrigues"
+  export GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa-${userAccount}"
+  echo userAccount....:${userAccount}
+  echo userEmail......:${userEmail}
+  echo userKey........:~/.ssh/id_rsa-${userAccount}
+  return
 done
